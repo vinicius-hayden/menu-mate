7d93267a25062efa483d608d13e1bf524bd96154
vinicius-hayden <vinihaydenss@gmail.com>
2024-10-25 12:11:39 -0400

    feat: implement data hook to `Product` and `Category` API

---
+++ dev/apps/frontend/src/data/hooks/useCategories.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import { Category } from "@menumate/core";
+import { useEffect, useState } from "react";
+
+const urlBase = "http://localhost:3000";
+
+export default function useCategories() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  async function getCategories(): Promise<Category[]> {
+    const response = await fetch(`${urlBase}/categories/`);
+    console.log(response);
+    const categories = await response.json();
+    return categories ?? [];
+  }
+
+  async function getCategoryById(id: number): Promise<Category | null> {
+    const resp = await fetch(`${urlBase}/category/${id}`);
+    const category = await resp.json();
+    return category ?? null;
+  }
+
+  useEffect(() => {
+    getCategories().then(setCategories);
+  });
+
+  return {
+    categories,
+    getCategoryById,
+  };
+}

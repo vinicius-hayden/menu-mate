40f66bb7c8775983b7ad0bf5eb891026e1f6fbec
vinicius-hayden <vinihaydenss@gmail.com>
2024-10-28 14:35:31 -0400

    feat: implement dynamic page routing on `Header`

--- dev/apps/frontend/src/components/template/Header.tsx
+++ dev/apps/frontend/src/components/template/Header.tsx
@@ -5,33 +5,38 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavbarCollapse, NavbarToggle } from "react-bootstrap";
 import Logo from "../shared/Logo";
-// import useCategories from "@/data/hooks/useCategories";
 import "./Header.css";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Header() {
-  // const { categories } = useCategories();
+  const [currentHref, setCurrentHref] = useState<string>("/menu");
+  const [nextPageName, setNextPageName] = useState<string>("All Products");
+  const pathname = usePathname();
+
+  function defineHrefAndNextPage(): void {    
+    if (currentHref != "/" && pathname === "/menu") {
+      setCurrentHref("/");
+      setNextPageName("All categories");
+    }
+  }
+
+  defineHrefAndNextPage();
 
   return (
     <>
       <Navbar style={{ backgroundColor: "#76a5af" }} data-bs-theme="dark">
         <Container className="justify-between">
           <Logo />
-          <Navbar.Brand href="#home" className="text-white" id="logo-text">
+          <Navbar.Brand href="/" className="text-white" id="logo-text">
             MenuMate
           </Navbar.Brand>
           <NavbarToggle aria-controls="basic-navbar-nav" />
           <NavbarCollapse id="basic-navbar-nav" className="justify-end">
             <Nav className="ml-auto">
-              <Nav.Link href="#home" className="text-white">
-                All Categories
+              <Nav.Link href={currentHref} className="text-white">
+                {nextPageName}
               </Nav.Link>
-              {/* <ul className="flex space-x-4">
-                {categories.map((category) => (
-                  <li key={category.id} className="text-white">
-                    {category.name}
-                  </li>
-                ))}
-              </ul> */}
             </Nav>
           </NavbarCollapse>
         </Container>

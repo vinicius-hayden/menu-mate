42022503699760d8506963f5eaf74f49e9099b3a
vinicius-hayden <vinihaydenss@gmail.com>
2024-11-01 18:12:39 -0400

    feat: add `Sidebar` and successful `Category` fetching to `Header`

--- dev/apps/frontend/src/components/template/Header.tsx
+++ dev/apps/frontend/src/components/template/Header.tsx
@@ -3,19 +3,23 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavbarToggle } from "react-bootstrap";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import Logo from "../shared/Logo";
 import "./Header.css";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import useCategories from "@/data/hooks/useCategories";
 
 export default function Header() {
   const [currentHref, setCurrentHref] = useState<string>("/menu");
   const [nextPageName, setNextPageName] = useState<string>("All Products");
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false); // Sidebar open/close state
+  const expand = false;
   const pathname = usePathname();
+  const { categories } = useCategories();
 
   function defineHrefAndNextPage(): void {
-    if (currentHref != "/" && pathname === "/menu") {
+    if (currentHref !== "/" && pathname === "/menu") {
       setCurrentHref("/");
       setNextPageName("All categories");
     }
@@ -23,22 +27,60 @@ export default function Header() {
 
   defineHrefAndNextPage();
 
+  function handleSidebarToggle(isOpen: boolean) {
+    setIsSidebarOpen(isOpen);
+  }
+
   return (
     <>
       <Navbar style={{ backgroundColor: "#76a5af" }} expand="lg">
-        <Container className="justify-between">
+        <Container fluid className="justify-between">
           <Logo />
           <Navbar.Brand href="/" className="text-white" id="logo-text">
             MenuMate
           </Navbar.Brand>
-          <NavbarToggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="header-items">
-            <Nav className="ms-auto">
-              <Nav.Link href={currentHref} className="text-white">
-                {nextPageName}
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-expand-${expand}`}
+            onClick={() => handleSidebarToggle(true)}
+          />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+            style={{ backgroundColor: "#76a5af" }}
+            show={isSidebarOpen}
+            onHide={() => handleSidebarToggle(false)}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title
+                id={`offcanvasNavbarLabel-expand-${expand}`}
+                className="text-white"
+              >
+                MenuMate
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="/" className="text-white text-xl">
+                  Home
+                </Nav.Link>
+                <Nav.Link href={currentHref} className="text-white text-xl">
+                  {nextPageName}
+                </Nav.Link>
+                {isSidebarOpen
+                  ? categories.map((category) => (
+                      <Nav.Link
+                        href="/some-path"
+                        className="text-white"
+                        key={category.name}
+                      >
+                        {category.name}
+                      </Nav.Link>
+                    ))
+                  : null}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
         </Container>
       </Navbar>
     </>
---
+++ dev/apps/frontend/src/components/template/HeaderIdeal.tsx
@@ -0,0 +1,38 @@
+'use client'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+export default function HeaderIdeal() {
+  return (
+    <>
+      {[true].map((expand) => (
+        <Navbar key={1} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  MenuMate
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}

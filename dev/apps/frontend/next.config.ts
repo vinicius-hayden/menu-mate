f14daccdd65e545f30d9f01feb51219dcfb2d205
vinicius-hayden <vinihaydenss@gmail.com>
2024-11-07 16:11:29 -0500

    fix: attempt to correct `Next` `Image` component issue

--- dev/apps/frontend/next.config.ts
+++ dev/apps/frontend/next.config.ts
@@ -6,9 +6,23 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
+        hostname: "s7d1.scene7.com",
+        port: '',
+        pathname: '/is/image/**',  // Match any path under /is/image/
+      },
+      {
+        protocol: "https",
+        hostname: "arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com",
+        port: '',
+        pathname: '/public/**',  // Allows images under /public/
       },
     ],
+  },
+  experimental: {
+    // Increase timeout to handle slow-loading images
+    images: {
+      timeout: 10000,  // in milliseconds (10 seconds)
+    },
   },
   sassOptions: {
     quietDeps: true, // Suppresses deprecation warnings from dependencies

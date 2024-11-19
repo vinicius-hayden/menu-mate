183ac4e669d6424a217c8a83e8b647ac47089d3b
vinicius-hayden <vinihaydenss@gmail.com>
2024-11-19 14:13:50 -0500

    fix: new `next.config.js` configuration to hardproof vercel deployment

--- dev/apps/frontend/next.config.js
+++ dev/apps/frontend/next.config.js
@@ -1,9 +1,13 @@
-// next.config.js
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+  webpack(config) {
+    // Resolve the '@' alias to the 'src' directory
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@menumate/core': path.resolve(__dirname, '../../packages/core/src') // Ensure this path points correctly to your core package
+    };
     return config;
   },
 };

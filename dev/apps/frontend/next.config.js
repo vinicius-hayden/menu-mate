c08a1d4702e43334edfec1dc208fba0ae4e290ec
vinicius-hayden <vinihaydenss@gmail.com>
2024-11-19 17:11:53 -0500

    ci: attempt on locating `menumate@core` root folder onto vercel deployment

--- dev/apps/frontend/next.config.js
+++ dev/apps/frontend/next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, 'src'),
-      '@menumate/core': path.resolve(__dirname, '../../packages/core/src') // Ensure this path points correctly to your core package
+      '@menumate/core': path.resolve(__dirname, '../../packages/core') // Ensure this path points correctly to your core package
     };
     return config;
   },

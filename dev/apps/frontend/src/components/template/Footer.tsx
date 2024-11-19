150252636d258e86bfe6bb8dc28649e0751c0642
vinicius-hayden <vinihaydenss@gmail.com>
2024-11-18 23:44:17 -0500

    style: old `Footer` removed

--- dev/apps/frontend/src/components/template/Footer.tsx
+++ dev/apps/frontend/src/components/template/Footer.tsx
@@ -13,42 +13,27 @@ export default function Footer() {
   return (
     <>
       <Navbar style={{ backgroundColor: "#76a5af" }} expand="lg">
-        <Container>
+        <Container className="flex flex-1 justify-end">
           <Logo />
-          <Navbar.Brand href="#home" className="text-white">
-            MenuMate
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home" className="text-white">
-                Home
-              </Nav.Link>
-              <Nav.Link href="#about" className="text-white">
-                About
-              </Nav.Link>
-              <Nav.Link href="#services" className="text-white">
-                Services
-              </Nav.Link>
-              <Nav.Link href="#contact" className="text-white">
-                Contact
-              </Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="https://youtube.com" target="_blank">
-                <IconBrandYoutube size={28} stroke={1} color="white" />
-              </Nav.Link>
-              <Nav.Link href="https://instagram.com" target="_blank">
-                <IconBrandInstagram size={28} stroke={1} color="white" />
-              </Nav.Link>
-              <Nav.Link href="https://facebook.com" target="_blank">
-                <IconBrandFacebook size={28} stroke={1} color="white" />
-              </Nav.Link>
-              <Nav.Link href="https://linkedin.com" target="_blank">
-                <IconBrandLinkedin size={28} stroke={1} color="white" />
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <div className="social-medias">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <Nav.Link href="https://youtube.com" target="_blank">
+                  <IconBrandYoutube size={28} stroke={1} color="white" />
+                </Nav.Link>
+                <Nav.Link href="https://instagram.com" target="_blank">
+                  <IconBrandInstagram size={28} stroke={1} color="white" />
+                </Nav.Link>
+                <Nav.Link href="https://facebook.com" target="_blank">
+                  <IconBrandFacebook size={28} stroke={1} color="white" />
+                </Nav.Link>
+                <Nav.Link href="https://linkedin.com" target="_blank">
+                  <IconBrandLinkedin size={28} stroke={1} color="white" />
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
         </Container>
       </Navbar>
     </>

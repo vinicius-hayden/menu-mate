0e214939be662c772938006a3ebb9346a18b1b0d
vinicius-hayden <vinihaydenss@gmail.com>
2024-11-23 20:45:13 -0500

    ci: refactor `ShoppingCartItem` and `ShoppingCartTotal`

--- dev/apps/frontend/src/components/checkout/ShoppingCartItem.tsx
+++ dev/apps/frontend/src/components/checkout/ShoppingCartItem.tsx
@@ -1,57 +1,47 @@
 /* eslint-disable @next/next/no-img-element */
-import { IconMinus, IconPlus, IconTrash } from '@tabler/icons-react'
-import { ShoppingCartItem as ShoppingCartModel } from '@menumate/core'
+import { IconMinus, IconPlus, IconTrash } from "@tabler/icons-react";
+import { ShoppingCartItem as ShoppingCartModel } from "@menumate/core";
 
 export interface ShoppingCartItemProps {
-    item: ShoppingCartModel
-    addItem: () => void
-    removeItem: () => void
-    removeProduct: () => void
+  item: ShoppingCartModel;
+  addItem: () => void;
+  removeItem: () => void;
+  removeProduct: () => void;
 }
 
 export default function ShoppingCartItem(props: ShoppingCartItemProps) {
-    const { item } = props;
-    const { product, quantity } = item
+  const { item } = props;
+  const { product, quantity } = item;
 
-    return (
-        <div className="flex items-center px-8 py-0 rounded-xl gap-16">
-            <img
-                src={product.image}
-                width={200}
-                height={0}
-                alt="image product"
-            />
-            <div className="flex flex-col h-28 flex-1">
-                <span className="text-xl">{product.name}</span>
-            </div>
-            <div className="flex flex-col items-center gap-4">
-                <span className="text-sm ">Quantity</span>
-                <div className="flex items-center border border-zinc-300 rounded-lg">
-                    <button
-                        disabled={quantity === 1}
-                        className={`${quantity === 1 && 'cursor-not-allowed'} px-2 py-0.5`}
-                        onClick={props.removeItem}
-                    >
-                        <IconMinus size={15} />
-                    </button>
-                    <span className="border-x  text-lg px-4 py-0.5">
-                        {quantity}
-                    </span>
-                    <button
-                        className="px-2 py-0.5 text-blue-500"
-                        onClick={props.addItem}
-                    >
-                        <IconPlus size={15} />
-                    </button>
-                </div>
-                <button
-                    className="flex items-center gap-1 text-red-600 select-none"
-                    onClick={props.removeProduct}
-                >
-                    <IconTrash size={20} />
-                    <span className="text-sm">Remove</span>
-                </button>
-            </div>
+  return (
+    <div className="flex items-center px-8 py-0 rounded-xl gap-16">
+      <img src={product.image} width={100} height={0} alt="image product" />
+      <div className="flex flex-col h-28 flex-1">
+        <span className="text-xl">{product.name}</span>
+      </div>
+      <div className="flex flex-col items-center gap-4">
+        <span className="text-sm ">Quantity</span>
+        <div className="flex items-center border border-zinc-300 rounded-lg">
+          <button
+            disabled={quantity === 1}
+            className={`${quantity === 1 && "cursor-not-allowed"} px-2 py-0.5`}
+            onClick={props.removeItem}
+          >
+            <IconMinus size={15} />
+          </button>
+          <span className="border-x  text-lg px-4 py-0.5">{quantity}</span>
+          <button className="px-2 py-0.5 text-blue-500" onClick={props.addItem}>
+            <IconPlus size={15} />
+          </button>
         </div>
-    )
+        <button
+          className="flex items-center gap-1 text-red-600 select-none"
+          onClick={props.removeProduct}
+        >
+          <IconTrash size={20} />
+          <span className="text-sm">Remove</span>
+        </button>
+      </div>
+    </div>
+  );
 }
--- dev/apps/frontend/src/components/checkout/ShoppingCartTotal.tsx
+++ dev/apps/frontend/src/components/checkout/ShoppingCartTotal.tsx
@@ -1,4 +1,5 @@
 import { IconShoppingCart } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 
 export interface ShoppingCartTotalProps {
@@ -6,46 +7,58 @@ export interface ShoppingCartTotalProps {
   totalPrice: number;
 }
 
-const serviceFee : number = 4.99;
-let taxesFee;
-
 export default function ShoppingCartTotal(props: ShoppingCartTotalProps) {
-  const newTotalPrice = props.totalPrice.toFixed(2);
-  taxesFee = (props.totalPrice * 0.13)
-  const testVariable = taxesFee.toFixed(2);
-  const totalOrder = Number(newTotalPrice) +  Number(serviceFee) + Number(taxesFee);
-  return (
-    <>
-      <h1>Order Summary</h1>
-      <div>
-        <div className="flex justify-between">
-          <h5>Subtotal</h5>
-          <span>${newTotalPrice}</span>
-        </div>
-        <div className="flex justify-between">
-          <h5>Service Fee</h5>
-          <span>${props.qtyItems > 0 ? serviceFee : 0}</span>
-        </div>
-        <div className="flex justify-between">
-          <h5>Taxes & Other fees (HST)</h5>
-          <span>${testVariable}</span>
-        </div>
-      </div>
-      <div className="flex justify-end items-center gap-7 h-24 rounded-xl px-7">
-        <div className="flex flex-col">
-          <span className="text-zinc-400">
-            Total ({props.qtyItems} {props.qtyItems === 1 ? "item" : "itens"}):
-          </span>
-          <span className="text-2xl font-semibold">${totalOrder.toFixed(2)}</span>
-        </div>
+  const [serviceFee, setServiceFee] = useState(0.0);
+  const [totalProductPrice, setTotalProductPrice] = useState(0.0);
+  const [taxesFee, setTaxesFee] = useState(0.0);
+  const [totalOrderPrice, setTotalOrderPrice] = useState(0.0)
 
-        <Button variant="secondary" size="lg" href="/checkout/payment">
-          <div className="flex flex-1">
-            <IconShoppingCart size={22} className="mt-1" />
-            <span>Order</span>
+  useEffect(() => {
+    if (props.qtyItems > 0) {
+      setServiceFee(4.99);
+      setTaxesFee((props.totalPrice * 0.13));
+      setTotalProductPrice(Number(props.totalPrice.toFixed(2)));
+      setTotalOrderPrice(Number(totalProductPrice) + Number(serviceFee) + Number(taxesFee));
+    }
+  }, [props.qtyItems, props.totalPrice, serviceFee, taxesFee, totalProductPrice, totalOrderPrice]);
+
+  if (props.qtyItems > 0) {
+    return (
+      <>
+        <h1>Order Summary</h1>
+        <div>
+          <div className="flex justify-between">
+            <h5>Subtotal</h5>
+            <span>${totalProductPrice}</span>
           </div>
-        </Button>
-      </div>
-    </>
-  );
+          <div className="flex justify-between">
+            <h5>Service Fee</h5>
+            <span>${props.qtyItems > 0 ? serviceFee : 0}</span>
+          </div>
+          <div className="flex justify-between">
+            <h5>Taxes & Other fees (HST)</h5>
+            <span>${taxesFee.toFixed(2)}</span>
+          </div>
+        </div>
+        <div className="flex justify-end items-center h-12 rounded-xl">
+          <div className="flex flex-1 justify-between">
+            <span className="text-zinc-400">
+              Total ({props.qtyItems} {props.qtyItems === 1 ? "item" : "items"})
+            </span>
+            <span className="text-2xl font-semibold">${totalOrderPrice.toFixed(2)}</span>
+          </div>
+  
+        </div>
+          <Button variant="secondary" size="lg" href="/checkout/payment">
+            <div className="flex flex-1 justify-center items-center">
+              <IconShoppingCart size={22} className="" />
+              <span>Order</span>
+            </div>
+          </Button>
+      </>
+    );
+  } else {
+    return <></>
+  }
+  
 }

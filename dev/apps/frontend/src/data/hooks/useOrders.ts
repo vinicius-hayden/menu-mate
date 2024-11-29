1facd4ddd3137fee02d057e253939559ec854759
vinicius-hayden <vinihaydenss@gmail.com>
2024-11-29 16:54:46 -0500

    ci: add new migration file for changes on `Order`

---
+++ dev/apps/frontend/src/data/hooks/useOrders.ts
@@ -0,0 +1,11 @@
+'use client'
+
+// import { Order } from "@menumate/core"
+// import { useEffect, useState } from "react"
+
+const urlBase = "http://localhost:3000"
+
+export default function useOrders() {
+
+  console.log(urlBase);
+}

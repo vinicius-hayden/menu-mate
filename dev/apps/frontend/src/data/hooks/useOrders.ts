1828f08d95a7bb29b671fe0af73ebd4fb8d30f58
vinicius-hayden <vinihaydenss@gmail.com>
2024-12-01 14:38:27 -0500

    ci: add `Order` status methods

--- dev/apps/frontend/src/data/hooks/useOrders.ts
+++ dev/apps/frontend/src/data/hooks/useOrders.ts
@@ -1,11 +1,51 @@
 'use client'
-
-// import { Order } from "@menumate/core"
-// import { useEffect, useState } from "react"
+import { Order } from "@menumate/core"
+import { useEffect, useState } from "react"
 
 const urlBase = "http://localhost:3000"
 
 export default function useOrders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  async function getOrders(): Promise<Order[]> {
+    const response = await fetch(`${urlBase}/orders`);
+    const orders = await response.json();
+    return orders ?? [];
+  }
 
-  console.log(urlBase);
+  async function getPendingOrders() : Promise<Order[]> {
+    const response = await fetch(`${urlBase}/orders?status=pending`);
+    const orders = await response.json(); 
+    return orders ?? [];
+  }
+
+  async function getPrepairingOrders(): Promise<Order[]> {
+    const response = await fetch(`${urlBase}/orders?status=prepairing`);
+    const orders = await response.json();
+    return orders ?? [];
+  }
+
+  async function getReadyOrders(): Promise<Order[]> {
+    const response = await fetch(`${urlBase}/orders?status=ready`);
+    const orders = await response.json();
+    return orders ?? [];
+  }
+
+  async function getPickedUpOrders(): Promise<Order[]> {
+    const response = await fetch(`${urlBase}/orders?status=pickedup`);
+    const orders = await response.json();
+    return orders ?? [];
+  }
+
+  useEffect(() => {
+    getOrders().then(setOrders);
+  }, []);
+
+  return {
+    orders,
+    getPendingOrders,
+    getPrepairingOrders,
+    getReadyOrders,
+    getPickedUpOrders
+  }
+
 }
